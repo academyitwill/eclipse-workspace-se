c59a0a8a02cf253166ff74e77393afcbaa8cc3f9
academyitwill <academyitwill@gmail.com>
2023-12-26 16:22:14 +0900

    ..

--- 00.HelloJava/src/HelloJava3.java
+++ 00.HelloJava/src/HelloJava3.java
@@ -2,10 +2,10 @@
 public class HelloJava3 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello java2");
+		System.out.println("Hello java3");
 		// content assist :  ctrl + space
-		System.out.println("안녕 자바2");
-		System.out.println("반가워 자바2");
+		System.out.println("안녕 자바3");
+		System.out.println("반가워 자바3");
 
 	}
 
